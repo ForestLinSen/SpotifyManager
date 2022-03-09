51c82c09a5f911951b1dbc9ee6cac836d1bd1373
Wyman Lin <41566276+ForestLinSen@users.noreply.github.com>
2022-03-09 22:29:28 +0800

    album collection view

--- SpotifyClone/Controllers/Other/AlbumViewController.swift
+++ SpotifyClone/Controllers/Other/AlbumViewController.swift
@@ -11,12 +11,59 @@ class AlbumViewController: UIViewController {
     
     private let album: Album
     
+    private let collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewCompositionalLayout(sectionProvider: { sectionIndex, _ in
+            AlbumViewController.createLayoutSection()
+        }))
+        return collectionView
+    }()
+    
+    private var viewModels = [RecommendationCellViewModel]()
+    
+    static func createLayoutSection() -> NSCollectionLayoutSection{
+        // item
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(1.0)))
+        
+        item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+        
+        // group
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(0.1)),
+            subitem: item,
+            count: 1)
+        
+        // section
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return section
+    }
+    
     init(album: Album){
         self.album = album
         super.init(nibName: nil, bundle: nil)
         
-        APICaller.shared.getAlbumDetail(albumID: album.id) { _ in
-            
+        APICaller.shared.getAlbumDetail(albumID: album.id) {[weak self] result in
+            switch result{
+            case .success(let albumResponse):
+                
+                DispatchQueue.main.async {
+                    self?.viewModels = albumResponse.tracks.items.compactMap({
+                        return RecommendationCellViewModel(trackName: $0.name, artistName: $0.artists.first?.name ?? "Unknown", imageURL: nil)
+                    })
+                    
+                    self?.collectionView.reloadData()
+                }
+                
+                
+                
+            case .failure(_):
+                break
+            }
         }
     }
     
@@ -27,6 +74,36 @@ class AlbumViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
+        collectionView.register(RecommendedTrackCollectionViewCell.self, forCellWithReuseIdentifier: RecommendedTrackCollectionViewCell.identifier)
+        
+        view.addSubview(collectionView)
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
+    
+    override func viewDidLayoutSubviews() {
+        collectionView.frame = view.bounds
+    }
+    
+}
+
+extension AlbumViewController: UICollectionViewDataSource, UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModels.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecommendedTrackCollectionViewCell.identifier, for: indexPath) as? RecommendedTrackCollectionViewCell else{
+            return UICollectionViewCell()
+        }
+        
+        let viewModel = viewModels[indexPath.row]
+        
+        cell.configure(with: viewModel)
+        
+        return cell
+    }
+    
     
 }
---
+++ SpotifyClone/ViewModels/AlbumCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumCellViewModel.swift
+//  SpotifyClone
+//
+//  Created by Sen Lin on 9/3/2022.
+//
+
+import Foundation
+
+struct AlbumCellViewModel{
+    let name: String
+    let artist: String
+}
