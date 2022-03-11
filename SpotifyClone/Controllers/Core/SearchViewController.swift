bf96088e716f79ddc6a3a1f74b7daa678f063da3
Wyman Lin <41566276+ForestLinSen@users.noreply.github.com>
2022-03-11 18:48:35 +0800

    searchBar controller

--- SpotifyClone/Controllers/Core/SearchViewController.swift
+++ SpotifyClone/Controllers/Core/SearchViewController.swift
@@ -7,14 +7,34 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UISearchResultsUpdating {
+
+    private let searchController: UISearchController = {
+        
+        let results = UIViewController()
+        results.view.backgroundColor = .systemBrown
+        let vc = UISearchController(searchResultsController: results)
+        vc.searchBar.placeholder = "Songs, Artists, Albums..."
+        vc.searchBar.searchBarStyle = .minimal
+        vc.definesPresentationContext = true
+        
+        return vc
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
-
+        view.backgroundColor = .systemBackground
+        searchController.searchResultsUpdater = self
         
+        navigationItem.searchController = searchController
     }
     
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else {
+            return
+        }
+        
+        print("Debug: search controller text: \(text)")
+    }
 
 }
--- SpotifyClone/Controllers/Core/TabBarViewController.swift
+++ SpotifyClone/Controllers/Core/TabBarViewController.swift
@@ -16,14 +16,6 @@ class TabBarViewController: UITabBarController {
         let vc2 = SearchViewController()
         let vc3 = LibraryViewController()
         
-        vc1.title = "Browse"
-        vc2.title = "Search"
-        vc3.title = "Library"
-        
-        vc1.navigationItem.largeTitleDisplayMode = .always
-        vc2.navigationItem.largeTitleDisplayMode = .always
-        vc3.navigationItem.largeTitleDisplayMode = .always
-        
         let nav1 = UINavigationController(rootViewController: vc1)
         let nav2 = UINavigationController(rootViewController: vc2)
         let nav3 = UINavigationController(rootViewController: vc3)
@@ -35,6 +27,12 @@ class TabBarViewController: UITabBarController {
         nav1.navigationBar.prefersLargeTitles = true
         nav2.navigationBar.prefersLargeTitles = true
         nav3.navigationBar.prefersLargeTitles = true
+        vc1.title = "Browse"
+        vc2.title = "Search"
+        vc3.title = "Library"
+        vc1.navigationItem.largeTitleDisplayMode = .always
+        vc2.navigationItem.largeTitleDisplayMode = .always
+        vc3.navigationItem.largeTitleDisplayMode = .always
         
         self.setViewControllers([nav1, nav2, nav3], animated: false)
         
